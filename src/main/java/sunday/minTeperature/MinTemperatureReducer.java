5126ee5266afa838ae3607fe762e4891ddcf956a
zhangjianang <1364487228@qq.com>
2017-06-04 23:21:29 +0800

    添加例子更新

--- src/main/java/sunday/minTeperature/MinTemperatureReducer.java
+++ src/main/java/sunday/minTeperature/MinTemperatureReducer.java
@@ -1,7 +1,22 @@
 package sunday.minTeperature;
-
+import java.io.IOException;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
 /**
  * Created by ang on 2017/6/4.
  */
-public class MinTemperatureReducer {
+
+
+public class MinTemperatureReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
+
+    @Override
+    public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
+
+        int minValue = Integer.MAX_VALUE;
+        for(IntWritable value : values) {
+            minValue = Math.min(minValue, value.get());
+        }
+        context.write(key, new IntWritable(minValue));
+    }
 }
